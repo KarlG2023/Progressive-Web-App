059464da125d18d65a92642967e0f772e497bb58
Yann <47141147+yann1906@users.noreply.github.com>
2022-10-27 16:40:12 +0200

    add test deploy

--- blablasneakers/src/App.js
+++ blablasneakers/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello on essaie de publier l'app via netlity
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
