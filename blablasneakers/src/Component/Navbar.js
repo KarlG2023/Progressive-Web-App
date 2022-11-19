d1ec99f0f4e6d35a72b2397fe514be1359a4e20c
Yann <47141147+yann1906@users.noreply.github.com>
2022-11-19 16:41:58 +0100

    update navbar.js

--- blablasneakers/src/Component/Navbar.js
+++ blablasneakers/src/Component/Navbar.js
@@ -14,7 +14,7 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import { mainListItems, secondaryListItems, thirdListItems } from '../Home/listItems';
 import Box from '@mui/material/Box';
-import { auth } from "../firebase";
+// import { auth } from "../firebase";
 
 const drawerWidth = 240;
 
@@ -147,8 +147,10 @@ export default function Navbar({ children }) {
         <List component="nav">
           {mainListItems}
           <Divider sx={{ my: 1 }} />
-            { auth ? secondaryListItems : thirdListItems }
-            {/* { auth && thirdListItems } */}
+          {secondaryListItems}
+          {thirdListItems}
+          {/* { auth ? secondaryListItems : thirdListItems } */}
+          {/* { auth && thirdListItems } */}
         </List>
       </Drawer>
 
