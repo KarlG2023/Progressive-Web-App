a9705424b7224374a56f45a3c1acdafb530938fc
Karl-Edouard <karl-edouard.glaser@epitech.eu>
2022-12-02 22:50:06 +0100

    fix AccountProfil error

--- blablasneakers/src/Profil/AccountProfil.js
+++ blablasneakers/src/Profil/AccountProfil.js
@@ -16,21 +16,19 @@ import { query, collection, getDocs, where } from "firebase/firestore";
 export const AccountProfile = (props) => {
   const [user] = useAuthState(auth);
   const [name, setName] = useState("");
+  var refresh = true;
 
   useEffect(() => {
-    // console.log(user)
     const fetchUserName = async () => {
-      try {
         const q = query(collection(db, "users"), where("uid", "==", user?.uid));
         const doc = await getDocs(q);
         const data = doc.docs[0].data();
         setName(data.name);
-      } catch (err) {
-        console.error(err);
-        alert("An error occured while fetching user data");
-      }
     };
-    fetchUserName();
+    if (refresh === true) {
+      refresh = false;
+      fetchUserName();
+    }
   }, [user]);
 
   return (
